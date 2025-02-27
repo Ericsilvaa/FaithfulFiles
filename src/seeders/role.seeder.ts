b6d3b7191148917b3c7c9e4ff0ae442ebf3fc834
Eric <eriicsilva28@gmail.com>
2025-02-27 19:23:54 -0300

    fix: reajust of imports

--- src/seeders/role.seeder.ts
+++ src/seeders/role.seeder.ts
@@ -1,16 +1,13 @@
-import { dbDataSourcePostgres } from '../database/db/dataSource';
-import { Book } from '../database/entities/postgres/book.entity';
-import { Role } from '../database/entities/postgres/roles.entity';
-
-
+import { dbDataSourcePostgres } from "../database/db/dataSource";
+import { Book } from "../entities/books/books.entity";
+import { RoleEntity } from "../entities/permissions/roles.entity";
 
 dbDataSourcePostgres.initialize().then(() => {
-  const repositoryRole = dbDataSourcePostgres.getRepository(Role)
-  const repositoryBook = dbDataSourcePostgres.getRepository(Book)
+  const repositoryRole = dbDataSourcePostgres.getRepository(RoleEntity);
+  const repositoryBook = dbDataSourcePostgres.getRepository(Book);
 
-  const roles = ['admin', 'default', 'owner'].forEach(async (role) => {
-    const roleEntity = repositoryRole.create({ name: role })
-    await repositoryRole.save(roleEntity)
-  })
-
-})
+  const roles = ["admin", "default", "owner"].forEach(async (role) => {
+    const roleEntity = repositoryRole.create({ name: role });
+    await repositoryRole.save(roleEntity);
+  });
+});
