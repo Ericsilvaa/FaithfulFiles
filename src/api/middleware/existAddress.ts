d8fc3b64dac48d8e27b70692916147fb28842f82
Eric <eriicsilva28@gmail.com>
2025-03-01 18:45:10 -0300

    chore: remove deprecated controllers, middleware, and routes

--- src/api/middleware/existAddress.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express"
-
-export const isAddress = (req: Request, res: Response, next: NextFunction) => {
-  const { address } = req.userLogged
-
-  if (!address) return res.status(401).json({ message: 'Conclua o cadastro! Cadastre seu endereço!' }).redirect('/library/profile')
-
-  return next()
-}
