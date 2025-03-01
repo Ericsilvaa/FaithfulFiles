d008b5112616364168eb7a0ba039b460d9253e03
Eric <eriicsilva28@gmail.com>
2025-03-01 11:54:51 -0300

    ♻️ refactor(routes): update index.routes.ts

--- src/api/routes/index.routes.ts
+++ src/api/routes/index.routes.ts
@@ -7,15 +7,7 @@ import PublisherRoute from "./publisher.routes";
 import RoleRoute from "./role.routes";
 import AdminRoute from "./admin.routes";
 
-import PostgresStrategy from "../../database/strategies/postgres/postgres.strategy";
-
 const router = (app: express.Router) => {
-  async function dbConnection() {
-    const connection = PostgresStrategy.connect();
-    return connection;
-  }
-  dbConnection();
-
   app.use("/library/profile", UserRoute);
   app.use("/library/auth/role", RoleRoute);
   app.use("/library/admin", AdminRoute);
