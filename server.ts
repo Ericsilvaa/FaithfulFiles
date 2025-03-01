4231186be9c85ff5b5b3c040dac789d71fcfa373
Eric <eriicsilva28@gmail.com>
2025-03-01 11:54:57 -0300

    � refactor(server): optimize server entry point

--- server.ts
+++ server.ts
@@ -1,16 +1,16 @@
-import server from "./src/app";
+import { createApp } from "./src/app";
+import PostgresStrategy from "./src/database/strategies/postgres/postgres.strategy";
 
-const app = server()
-const port = app.get('port')
+const app = createApp();
 
-app.listen(port, () => {
-  console.log("🚀 [server]: listening on port " + port);
-});
-
-
-
-
-
-
-
-
+PostgresStrategy.connect()
+  .then(() => {
+    const port = app.get("port");
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("❌ Failed to connect to the database:", error);
+    process.exit(1);
+  });
